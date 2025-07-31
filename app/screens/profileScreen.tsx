80fb3d58c39789038c5b3414dfe26694d5029a2b
Abhishek Agrahari <devabhishekagrahari@gmail.com>
2025-07-31 14:44:23 +0530

    Profile Section

--- app/screens/profileScreen.tsx
+++ app/screens/profileScreen.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import {  Text, View } from "react-native";
+import {  ScrollView, Text, View } from "react-native";
 import CheckBalanceCard from "../components/Profile/checkBalance";
 import SettingsCompo from "../components/Profile/settings";
 
 
 const ProfileScreen=()=>{
     return(
-        <View  style={{flex:1, padding:16,gap:20}}>
+        <ScrollView  style={{flex:1, padding:16,gap:20}}>
             <Text>Profile</Text>
             <CheckBalanceCard/>
             <SettingsCompo/>
-        </View>
+        </ScrollView>
     )
 }
 
