8784db7ec91a8ba796e595473f1be7f96eb2657b
Abhishek Agrahari <devabhishekagrahari@gmail.com>
2025-08-07 11:40:09 +0530

    7 aug subh ke 11:40 bje

--- App.tsx
+++ App.tsx
@@ -35,7 +35,7 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Splash" component={SplashScreen} />
-        <Stack.Screen name="Splash2" component={addCardDetails} />
+        <Stack.Screen name="Splash2" component={selectYourBankScreen} />
         <Stack.Screen name="primaryRole" component={PrimaryRoleScreen} /> 
         <Stack.Screen name="secondaryRole" component={SecondaryRoleScreen} />
         <Stack.Screen name="selectLanguage" component={SelectLanguageScreen}/>
